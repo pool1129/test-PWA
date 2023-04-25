f6d7bd0b17bd23710adf3bdd803330c1c1216317
wwl-jky <jky@winwinlab.co.kr>
2023-04-25 17:03:16 +0900

    [fix] file link address

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'offline';
-const OFFLINE_URL = 'offline.html';
+const CACHE_NAME = './offline';
+const OFFLINE_URL = './offline.html';
 
 self.addEventListener('install', function(event) {
   console.log('[ServiceWorker] Install');
