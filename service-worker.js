8e23f11323d443c520843a4f69c466e8a058c83d
wwl-jky <jky@winwinlab.co.kr>
2023-04-25 17:36:31 +0900

    [fix] sw.js

--- service-worker.js
+++ service-worker.js
@@ -1,52 +1,46 @@
-const CACHE_NAME = 'offline';
-const OFFLINE_URL = './offline.html';
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+      navigator.serviceWorker.register("./service-worker.js");
+  });
+}
 
-self.addEventListener('install', function(event) {
-  console.log('[ServiceWorker] Install');
-  
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    // Setting {cache: 'reload'} in the new request will ensure that the response
-    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
-  })());
-  
-  self.skipWaiting();
+// 캐시 이름
+const CACHE_NAME = "cache-v1";
+
+// 캐싱할 파일
+const FILES_TO_CACHE = [
+    "./offline.html"
+];
+
+// 상술한 파일 캐싱
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('[ServiceWorker] Activate');
-  event.waitUntil((async () => {
-    // Enable navigation preload if it's supported.
-    // See https://developers.google.com/web/updates/2017/02/navigation-preload
-    if ('navigationPreload' in self.registration) {
-      await self.registration.navigationPreload.enable();
-    }
-  })());
-
-  // Tell the active service worker to take control of the page immediately.
-  self.clients.claim();
+// CACHE_NAME이 변경되면 오래된 캐시 삭제
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) =>
+            Promise.all(
+                keyList.map((key) => {
+                    if (CACHE_NAME !== key) return caches.delete(key);
+                })
+            )
+        )
+    );
 });
 
-self.addEventListener('fetch', function(event) {
-  // console.log('[Service Worker] Fetch', event.request.url);
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResponse = await event.preloadResponse;
-        if (preloadResponse) {
-          return preloadResponse;
-        }
+// 요청에 실패하면 오프라인 페이지 표시
+self.addEventListener("fetch", (event) => {
+    if ("navigate" !== event.request.mode) return;
 
-        const networkResponse = await fetch(event.request);
-        return networkResponse;
-      } catch (error) {
-        console.log('[Service Worker] Fetch failed; returning offline page instead.', error);
-
-        const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(OFFLINE_URL);
-        return cachedResponse;
-      }
-    })());
-  }
+    event.respondWith(
+        fetch(event.request).catch(() =>
+            caches
+                .open(CACHE_NAME)
+                .then((cache) => cache.match("/offline.html"))
+        )
+    );
 });
